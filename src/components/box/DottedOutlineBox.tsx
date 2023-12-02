fb6c5710df03af24e86a3232be01a8f21a62aa3b
Kota Ito <boooocchi@gmail.com>
2023-12-01 17:21:02 -0800

    adjusted animation

--- src/components/box/DottedOutlineBox.tsx
+++ src/components/box/DottedOutlineBox.tsx
@@ -19,15 +19,15 @@ const DottedOutlineBox: React.FC<dottedOutlineBoxProps> = ({
     <motion.div
       initial={{
         opacity: 0,
-        y: "30%"
+        y: "15%"
       }}
       whileInView={{
         opacity: 1,
         y: "0%",
         transition: {
-          duration: 0.4,
+          duration: 0.3,
           type: "spring",
-          stiffness: 150
+          stiffness: 100
         }
       }}
       viewport={{ once: true, amount: 0.3 }}
