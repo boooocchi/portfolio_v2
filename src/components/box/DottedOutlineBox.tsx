3782e41371773ffd0a850ba98201db0bf5cbf676
Kota Ito <boooocchi@gmail.com>
2023-12-01 17:48:21 -0800

    adjusted animation

--- src/components/box/DottedOutlineBox.tsx
+++ src/components/box/DottedOutlineBox.tsx
@@ -20,27 +20,31 @@ const DottedOutlineBox: React.FC<dottedOutlineBoxProps> = ({
       "flex flex-col  md:gap-5 outline-mainBlack  md:min-h-[600px]  h-[650px] w-[90%] md:w-[85%] outline-dotted outline-[1px] sm:outline-[2px] lg:mt-[2rem] px-[1.3rem] xs:px-[2rem] md:px-[4rem] py-[2rem] md:py-[3rem] relative px-[1.5rem] py-[1.5rem]";
   }
   return (
-    <motion.div
-      initial={
-        !smartPhone && {
-          opacity: 0,
-          x: `${lineColor === "mainBlack" ? "-10%" : "10%"}`
-        }
-      }
-      whileInView={{
-        opacity: 1,
-        x: "0%",
-        transition: {
-          duration: 0.5,
-          type: "spring",
-          stiffness: 100
-        }
-      }}
-      viewport={{ once: true, amount: 0.5 }}
-      className={dottedOutlineBoxClassName}
-    >
-      {children}
-    </motion.div>
+    <>
+      {!smartPhone ? (
+        <motion.div
+          initial={{
+            opacity: 0,
+            x: `${lineColor === "mainBlack" ? "-10%" : "10%"}`
+          }}
+          whileInView={{
+            opacity: 1,
+            x: "0%",
+            transition: {
+              duration: 0.5,
+              type: "spring",
+              stiffness: 100
+            }
+          }}
+          viewport={{ once: true, amount: 0.5 }}
+          className={dottedOutlineBoxClassName}
+        >
+          {children}
+        </motion.div>
+      ) : (
+        <div className={dottedOutlineBoxClassName}>{children}</div>
+      )}
+    </>
   );
 };
 
