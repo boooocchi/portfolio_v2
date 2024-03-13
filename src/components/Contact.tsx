32fe3dfb0967b5044f3b1b79e84232d13a85403e
Kota Ito <boooocchi@gmail.com>
2024-03-12 19:43:20 -0700

    change the layout of contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -215,7 +215,7 @@ const Contact = () => {
             </div>
             <div className="w-full md:w-[57%] flex flex-col justify-center items-center h-full ">
               <textarea
-                className={`xs:h-[13rem] sm:h-[10rem] md:h-full  w-full bg-patternGray p-3 md:p-8   text-mainBlack  focus:outline-none  mt-7 md:mt-0 rounded-none max-sm:text-[.9rem] ${currentFont.className}}`}
+                className={`h-[13rem] sm:h-[10rem] md:h-full  w-full bg-patternGray p-3 md:p-8   text-mainBlack  focus:outline-none  mt-7 md:mt-0 rounded-none max-sm:text-[.9rem] ${currentFont.className}}`}
                 placeholder={t("contact.messagePlaceHolder")}
                 {...register("message")}
               />
