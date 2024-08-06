1661e39b5746f667f9ee50e71c67b4fadb73b6eb
Kota Ito <boooocchi@gmail.com>
2024-08-06 16:54:45 -0700

    modified dotted outline box

--- src/components/layout/DottedOutlineBox.tsx
+++ src/components/layout/DottedOutlineBox.tsx
@@ -14,7 +14,7 @@ const DottedOutlineBox: React.FC<dottedOutlineBoxProps> = ({
     query: "(max-width: 480px)"
   });
   const basicClass =
-    "flex flex-col sm:h-[620px] h-[650px] w-[90%] sm:w-[85%] outline-dotted lg:mt-[2rem] outline-[2px] sm:py-[2.5rem] sm:px-[3.5rem] p-[1.3rem] relative ml-[10px]";
+    "flex flex-col sm:h-[620px] h-[650px] w-full sm:w-[85%] sm:outline-dotted lg:mt-[2rem] outline-[2px] sm:py-[2.5rem] sm:px-[3.5rem] p-[1.3rem] relative ml-[10px]";
 
   let dottedOutlineBoxClassName = "outline-fontGray ";
   if (lineColor === "mainBlue") {
@@ -25,13 +25,11 @@ const DottedOutlineBox: React.FC<dottedOutlineBoxProps> = ({
       <motion.div
         initial={{
           opacity: 0,
-          x: `${isSm ? "0%" : lineColor === "mainBlue" ? "-5%" : "5%"}`,
-          y: `${isSm && "5%"}`
+          x: `${isSm ? "0%" : lineColor === "mainBlue" ? "-5%" : "5%"}`
         }}
         whileInView={{
           opacity: 1,
           x: "0%",
-          y: "0%",
           transition: {
             duration: 0.3,
             stiffness: 100
