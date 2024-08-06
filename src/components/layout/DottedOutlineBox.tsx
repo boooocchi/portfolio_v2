385bc533cb76b65b06ea302783f0fc941cea0899
Kota Ito <boooocchi@gmail.com>
2024-08-06 16:15:19 -0700

    modified dotted outline box

--- src/components/layout/DottedOutlineBox.tsx
+++ src/components/layout/DottedOutlineBox.tsx
@@ -10,12 +10,8 @@ const DottedOutlineBox: React.FC<dottedOutlineBoxProps> = ({
   children,
   lineColor
 }) => {
-  const smartPhone = useMediaQuery({
-    query: "(max-width: 480px)"
-  });
-
   const basicClass =
-    "flex flex-col sm:h-[620px]  h-[650px] w-[90%] sm:w-[85%] outline-dotted  lg:mt-[2rem] sm:outline-[2px] sm:py-[2.5rem] sm:px-[3.5rem] py-[1.3rem]  px-[1.3rem] relative ";
+    "flex flex-col sm:h-[620px] h-[650px] w-[85%] sm:w-[85%] outline-dotted lg:mt-[2rem] outline-[2px] sm:py-[2.5rem] sm:px-[3.5rem] py-[1.3rem] px-[1.3rem] relative";
 
   let dottedOutlineBoxClassName = "outline-fontGray ";
   if (lineColor === "mainBlue") {
