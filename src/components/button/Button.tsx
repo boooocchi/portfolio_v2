efcaaecc66b40e4cd87d177a728e63f966c4c4f4
Kota Ito <boooocchi@gmail.com>
2024-08-07 19:19:13 -0700

    modified the css implementation of button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -24,17 +24,18 @@ const Button: React.FC<ButtonProps> = ({
   const currentFont = i18n.language === "ja" ? japanese : hind;
   return (
     <button
-      className={`flex justify-center border-transparent border-2 items-center hover:border-2 hover:border-mainBlue ${
-        bgColor ? bgColor : "bg-mainYellow"
-      } text-center px-3 py-2 md:px-3 hover:bg-patternGray ${
-        bgColor ? "text-fontFray hover:text-mainBlue" : "text-mainBlue"
-      } transition-colors duration-300  ${additionalClassName} ${
-        currentFont.className
-      } tracking-tighter text-[.8rem] xs:text-[.9rem]`}
+      className={`flex justify-center border-transparent border-2 items-center hover:border-2 hover:border-mainBlue text-center px-3 py-3 md:px-3 hover:bg-patternGray transition-colors duration-300 tracking-tighter text-[.8rem] xs:text-[.9rem]  
+      ${additionalClassName}
+      ${currentFont.className}
+       ${
+         bgColor
+           ? bgColor + "text-fontGray hover:text-mainBlue"
+           : "bg-mainYellow text-mainBlue"
+       } `}
       disabled={isSubmitting}
       onClick={onClick}
     >
-      <span className="mt-[0.5px]">{children}</span>
+      <span className="leading-[100%]">{children}</span>
     </button>
   );
 };
